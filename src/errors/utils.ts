9a4cd1eb49b122d232784a6f19c0e199f97871ad
jcurbelo <curbelorobin@gmail.com>
2022-02-01 16:51:29 -0500

    Checks for Polygon 'insufficient funds' error as well

--- src/errors/utils.ts
+++ src/errors/utils.ts
@@ -1,7 +1,12 @@
 import { EthereumRpcError } from 'eth-rpc-errors'
 
 export function handleError(e: EthereumRpcError<unknown>): void {
-  const msg = e.message
+  // tries to parse Internal JSON RPC error, see https://eips.ethereum.org/EIPS/eip-1474#error-codes
+  if (e.code === -32603) {
+    e = e.data as EthereumRpcError<unknown>
+  }
+
+  const msg = e.message || 'Something went wrong.'
   // checks if the user has metamask installed
   if (msg.includes('missing provider') || msg.includes('No provider found')) {
     throw new Error(
@@ -9,11 +14,11 @@ export function handleError(e: EthereumRpcError<unknown>): void {
     )
   }
 
-  // ideally we would check for the error code here, but the same error code (-32000)
-  // is used for more than one error type.
+  // ideally we would check for the error code here, but the same error code: -32000 (Bad Input)
+  // is used for more than one error type
   if (msg.includes('err: insufficient funds for gas * price + value')) {
-    throw new Error('You do not have enough balance.')
+    throw new Error('Your wallet does not have enough balance.')
   }
 
-  throw Error(e.message)
+  throw Error(msg)
 }
