fc309a0ffa0a95ed07e6ed7e1720970cfc4b381e
jcurbelo <curbelorobin@gmail.com>
2022-02-11 17:05:05 -0500

    Adds new production endpoint

--- src/config/endpoint.ts
+++ src/config/endpoint.ts
@@ -1,2 +1,2 @@
 export const API_ENDPOINT_DEV = 'https://api-testnet.niftykit.com'
-export const API_ENDPOINT = 'https://app.niftykit.com/api'
+export const API_ENDPOINT = 'https://api.niftykit.com'
